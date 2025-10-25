7611a503fd6ed243cf307e1791b2472863f23f19
Heidi Holappa <heidi.holappa@helsinki.fi>
2025-10-25 09:52:24 +0300

    Add solution to exercise 10.12

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { NativeRouter } from 'react-router-native';
 import { ApolloProvider } from '@apollo/client';
+import Constants from 'expo-constants';
 
 import Main from './src/components/Main';
 
@@ -8,15 +9,20 @@ import createApolloClient from './src/utils/apolloClient';
 
 const apolloClient = createApolloClient();
 
-const App = () => (
-  <>
-    <NativeRouter future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
-        <ApolloProvider client={apolloClient}>
-          <Main />
-        </ApolloProvider>
-    </NativeRouter>
-    <StatusBar style="auto" />
-  </>
-);
+const App = () => {
+
+  console.log(Constants.expoConfig);
+
+  return (
+    <>
+      <NativeRouter future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
+          <ApolloProvider client={apolloClient}>
+            <Main />
+          </ApolloProvider>
+      </NativeRouter>
+      <StatusBar style="auto" />
+    </>
+  );
+};
 
 export default App;
